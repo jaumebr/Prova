8e161246d78a56c39b2332593e769aa08a40497e
jbr <superbew@gmail.com>
2012-02-07 16:48:20 +0100

    He començat una nova branca

--- prova.java
+++ prova.java
@@ -1,6 +1,6 @@
 public class prova{
 	public static void main(String[] args){
-		System.out.print("Això és una prova");
+		System.out.print("Això és una prova a la nova branca");
 		System.out.println("-");
 	}
 }
